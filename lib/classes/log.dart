697687d481f5b3c1609f9c5eac2ec349b82ccb64
Dusan Todorovic <dtodorovic40@gmail.com>
2023-02-19 18:19:03 +0100

    feat: logs alert

--- lib/classes/log.dart
+++ lib/classes/log.dart
@@ -1,7 +1,11 @@
+import 'package:demeter_huawei/classes/spray_drone_icon_icons.dart';
+import 'package:flutter/material.dart';
+
 enum LogAction {
-  sprayed,
-  scanned,
-  watered,
+  sprayed, // custom svg icon
+  scanned, // camera_alt constant
+  watered, // water_drop constant
+  alert,
 }
 
 class Log {
@@ -20,7 +24,7 @@ class Log {
   });
 
   Log.fromJson(Map<String, dynamic> json)
-      : action = getWeatherFromString(json["action"]),
+      : action = getIconFromString(json["action"]),
         text = json["text"],
         farm = json["farm"],
         droneId = json["droneId"],
@@ -37,11 +41,85 @@ class Log {
   DateTime dateTime() {
     return DateTime.fromMillisecondsSinceEpoch(time * 1000);
   }
+
+  Widget icon() {
+    switch (action) {
+      case LogAction.sprayed:
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
+            color: Color.fromARGB(255, 99, 183, 134),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              SprayDroneIcon.spray_robot_icon,
+              color: Colors.white,
+            ),
+          ),
+        );
+      case LogAction.scanned:
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
+            color: Color.fromARGB(255, 99, 183, 134),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.camera_alt,
+              color: Colors.white,
+            ),
+          ),
+        );
+      case LogAction.watered:
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
+            color: Color.fromARGB(255, 99, 183, 134),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.water_drop_rounded,
+              color: Colors.white,
+            ),
+          ),
+        );
+      case LogAction.alert:
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
+            color: Color.fromARGB(255, 99, 183, 134),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.dangerous,
+              color: Colors.white,
+            ),
+          ),
+        );
+    }
+  }
 }
 
-LogAction getWeatherFromString(String logType) {
+// Container(
+//               color: Colors.green,
+//               child: new IconButton(
+//                   icon: new Icon(Icons.search,color: Colors.white,),onPressed: null),
+//             ),
+LogAction getIconFromString(String logType) {
   for (LogAction element in LogAction.values) {
-    if (element.toString() == logType) {
+    if (element.toString() == "LogAction.$logType") {
       return element;
     }
   }
