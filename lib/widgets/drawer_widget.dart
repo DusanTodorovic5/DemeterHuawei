fbf38c2da9fa4bd0357660522d0b42cae8a21405
Dusan Todorovic <dtodorovic40@gmail.com>
2023-02-20 08:55:10 +0100

    style: drawer

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,3 +1,4 @@
+import 'package:demeter_huawei/classes/drone_farm_icons.dart';
 import 'package:flutter/material.dart';
 
 class DrawerWidget extends StatelessWidget {
@@ -6,23 +7,54 @@ class DrawerWidget extends StatelessWidget {
   final pages = [
     "Farms",
     "Logs",
+    "Drones",
     "Weather",
     "Preferences",
-    "Drones",
   ];
+
+  final icons = {
+    "Farms": DroneFarm.farm,
+    "Logs": Icons.history,
+    "Drones": DroneFarm.drone,
+    "Weather": Icons.cloud,
+    "Preferences": Icons.settings,
+  };
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.zero,
-      children: pages.map((entry) {
-        return ListTile(
-          title: Text(entry),
-          onTap: () {
-            Navigator.popAndPushNamed(context, "/$entry");
-          },
-        );
-      }).toList(),
+    return Padding(
+      padding: const EdgeInsets.only(top: 35),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: pages.map((entry) {
+          return ListTile(
+            leading: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(8),
+                ),
+                color: Color.fromARGB(255, 99, 183, 134),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  icons[entry],
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            title: Text(
+              entry,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            onTap: () {
+              Navigator.popAndPushNamed(context, "/$entry");
+            },
+          );
+        }).toList(),
+      ),
     );
   }
 }
